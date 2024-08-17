44837bfada1f860e810a621436711d036bf78d1e
league <league@imac-39.attlocal.net>
2024-08-17 12:24:43 -0700

    fcvxsv

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,31 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("uno");
+		list.add("dos");
+		list.add("tres");
+		list.add("quatro");
+		list.add("sinco");
 		// 2. Set one item in our list to be something different
-		
+		list.set(1, "hehe");
 		// 3. Remove something from our list
-		
+		list.remove(3);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		if(list.contains("sigma")) {
+			System.out.println("sigma is in the list!");
+		}
+		else {
+			System.out.println("sigma is not in the list.");
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(3));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,6 +1,15 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +18,36 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame;
+	JPanel panel;
+	JButton add;
+	ArrayList<String> list;
+	JButton view;
+	GuestBook(){
+		view = new JButton("view");
+		add = new JButton("add");
+		panel = new JPanel();
+		frame = new JFrame();
+		list = new ArrayList<String>();
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		frame.pack();
+		frame.setSize(200,200);
+		frame.setVisible(true);
+		add.addActionListener(this);
+		view.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==add) {
+			String name = JOptionPane.showInputDialog("Add person to list:");
+			list.add(name);
+		} else {
+			JOptionPane.showMessageDialog(null, list);
+		}
+
+	}
+
 }
